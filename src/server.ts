1702835eb74f1b0f7958bea047e13389485f2e1d
Marco m <marco.michelotti-1@studenti.unitn.it>
2018-04-09 17:50:54 +0200

    start server

--- src/server.ts
+++ src/server.ts
@@ -40,7 +40,8 @@ server.use(restify.plugins.throttle({
 
 server.use(restify.plugins.gzipResponse())
 
-server.listen(3000, function() {
+server.listen(3000, "http://suspicious-jang-b9a99e.netlify.com/", function() {
+    console.log(server.address())
     console.log('\t%s listening at %s', server.name, server.url);
 })
 
